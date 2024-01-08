8401eaf4fea2b7e02d2222099021e54170e0affe
Nacho Martinez Garrido <ignacio.martinez@gameloft.com>
2024-01-08 12:59:27 +0100

    Updated .gitignore

--- Readme.el
+++ Readme.el
@@ -208,7 +208,6 @@
 
 	(global-set-key "\C-s" 'swiper)
 	(global-set-key (kbd "C-c C-r") 'ivy-resume)
-	(global-set-key (kbd "<f6>") 'ivy-resume)
 	(global-set-key (kbd "M-x") 'counsel-M-x)
 	(global-set-key (kbd "M-f") 'counsel-find-file)
 	(global-set-key (kbd "<f1> f") 'counsel-describe-function)
@@ -217,11 +216,10 @@
 	(global-set-key (kbd "<f1> l") 'counsel-find-library)
 	(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
 	(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-	(global-set-key (kbd "C-c g") 'counsel-git)
 	(global-set-key (kbd "C-c j") 'counsel-git-grep)
 	(global-set-key (kbd "C-c k") 'counsel-grep)
 	(global-set-key (kbd "C-x l") 'counsel-locate)
-	(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+	;;(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 	(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 	))
 
